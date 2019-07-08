09c14b21c723d1a7e37378d1504fed652ff51195
Raviraghul2210 <52314446+Raviraghul2210@users.noreply.github.com>
2019-07-08 10:58:12 +0530

    Update and rename natural.py to program8.py

--- natural.py
+++
@@ -1,6 +0,0 @@
-no = input()
-no = int(no)
-sum = 0
-for addition in range(0,no+1):
-    sum = sum+addition
-print(sum)
---
+++ program8.py
@@ -0,0 +1,5 @@
+ri=int(input(""))
+sum=0
+for i in range(1,ri+1):
+    sum=sum+i
+print(sum)
